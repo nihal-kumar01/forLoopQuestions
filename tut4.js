2c7686a7fa757d10eccd46ddaacfd4bf65cab14e
Nihal Kumar <nihalkumarofficial1@gmail.com>
2026-01-17 12:49:29 +0530

    Implement sum calculation for array of numbers

---
+++ tut4.js
@@ -0,0 +1,11 @@
+// Complete the code to find the sum of all numbers in the array:
+
+let nums = [5, 10, 15];
+let sum = 0;
+
+for (let n of nums) {
+  // write your code here
+  sum += n;
+}
+
+console.log(sum); // should print 30
