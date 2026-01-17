14c6c23eadb37ed5d1d3beb61947a3bac234aac8
Nihal Kumar <nihalkumarofficial1@gmail.com>
2026-01-17 12:46:01 +0530

    Refactor fruit printing to use for...of loop
    Updated the fruit printing logic to simplify output.

---
+++ tut3.js
@@ -0,0 +1,9 @@
+// Given this array:
+
+let fruits = ["Apple", "Banana", "Mango"];
+// Use a for...of loop to print each fruit.
+
+for(let fruit of fruits)
+    {
+        console.log(fruit);
+    };
